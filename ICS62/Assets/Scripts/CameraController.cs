acdda09a4530e8cfe911007f50647bba7b5b1773
Tyler Pearce <pearcetyler@yahoo.com>
2016-04-27 14:19:32 -0700

    Added player
    Added player with movement and camera controller scripts

---
+++ ICS62/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController: MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+	private float offsetMagnitude;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+		offsetMagnitude = offset.magnitude;
+	}
+
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.rotation = player.transform.rotation;	//match rotation of player
+		transform.position = player.transform.position - transform.forward * 6 + transform.up * 3;	//match position of player
+		transform.LookAt (player.transform.position);
+	}
+}
---
+++ ICS62/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;	
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position += transform.forward * Time.deltaTime * speed;
+		}
+		if (Input.GetKey (KeyCode.Q)) {
+			transform.position -= transform.right * Time.deltaTime * speed * 0.7f;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.position -= transform.forward * Time.deltaTime * speed / 2;
+		}
+		if (Input.GetKey (KeyCode.E)) {
+			transform.position += transform.right * Time.deltaTime * speed * 0.7f;
+		}
+		if(Input.GetKey(KeyCode.A)){
+			transform.Rotate(Vector3.up, -speed * 25 * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.D)){
+			transform.Rotate(Vector3.up, speed * 25 * Time.deltaTime);
+		}
+				
+
+
+
+	}
+
+
+}
+	
