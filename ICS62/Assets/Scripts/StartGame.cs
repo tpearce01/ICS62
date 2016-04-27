704cd19cddf43e94f8433542dd9bd2ed003d1ba1
Tyler Pearce <pearcetyler@yahoo.com>
2016-04-27 14:24:52 -0700

    Added functionality to start and quit buttons
    Added functionality to start and quit buttons. Start loads level one.
    Quit will quit the application (Only works in build, not scene view)

--- ICS62/Assets/Scripts/StartGame.cs
+++ ICS62/Assets/Scripts/StartGame.cs
@@ -1,15 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public void LoadLevelOne(){
+		SceneManager.LoadScene ("LevelOne");
 	}
 }
