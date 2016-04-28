6052e1d3c7e90b007cdecac5065028d1cdbf04e1
Tyler Pearce <pearcetyler@yahoo.com>
2016-04-28 14:09:23 -0700

    Added instruction button functionality
    added instruction button functionality. Still need to actually add
    instructions

---
+++ ICS62/Assets/Scripts/InstructionsButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InstructionsButton : MonoBehaviour {
+
+	public Button startButton;
+	public Button instructionsButton;
+	public Button quitButton;
+	public Button returnButton;
+
+	public void clickMe() {
+		startButton.gameObject.SetActive (!startButton.IsActive ());
+		instructionsButton.gameObject.SetActive (!instructionsButton.IsActive ());
+		quitButton.gameObject.SetActive (!quitButton.IsActive ());
+		returnButton.gameObject.SetActive (!returnButton.IsActive ());
+	}
+		
+
+}
