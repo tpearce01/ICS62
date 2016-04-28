74dfb284555ef0addf09b22240d9811fbff9a1c7
Tyler Pearce <pearcetyler@yahoo.com>
2016-04-28 14:22:09 -0700

    Aesthetic changes
    Played around with stretching buttons when screen size changes

--- ICS62/Assets/Scripts/InstructionsButton.cs
+++ ICS62/Assets/Scripts/InstructionsButton.cs
@@ -8,12 +8,18 @@ public class InstructionsButton : MonoBehaviour {
 	public Button instructionsButton;
 	public Button quitButton;
 	public Button returnButton;
+	public Text instructionsText;
+	public Text instructionsTitle;
+	public Text titleText;
 
 	public void clickMe() {
 		startButton.gameObject.SetActive (!startButton.IsActive ());
 		instructionsButton.gameObject.SetActive (!instructionsButton.IsActive ());
 		quitButton.gameObject.SetActive (!quitButton.IsActive ());
 		returnButton.gameObject.SetActive (!returnButton.IsActive ());
+		instructionsText.gameObject.SetActive (!instructionsText.IsActive ());
+		instructionsTitle.gameObject.SetActive (!instructionsTitle.IsActive ());
+		titleText.gameObject.SetActive (!titleText.IsActive ());
 	}
 		
 
