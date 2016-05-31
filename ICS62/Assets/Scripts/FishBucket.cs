541ec3b2af2b08beeac41962d138807fcae75097
kngraym <raymok1@uci.edu>
2016-05-31 16:25:53 -0700

    Updated Credits and added bucket to level one plus scripting

---
+++ ICS62/Assets/Scripts/FishBucket.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishBucket : MonoBehaviour {
+
+	void SetTransformY(float n){
+		transform.position = new Vector3 (36, n, -3);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		//If collides with dock
+		if (other.CompareTag ("Dock")) {
+				//Put Fish in Bucket;
+				SetTransformY(3.0f);
+
+	Debug.Log ("Hello");
+		
+	}
+}
+}
