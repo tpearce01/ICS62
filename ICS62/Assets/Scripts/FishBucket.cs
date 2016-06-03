8104bfb96140aa4b97529a9a60f4c670569a33bc
Tyler Pearce <pearcetyler@yahoo.com>
2016-06-03 12:55:52 -0700

    Fixed fish in bucket
    Aligned colliders & expanded roof / added floor (to prevent fish
    halfway through the bottom of bucket). Fish were all moving to the same
    spot, causing them to shake uncontrollably. Adjusted fish mass and drag
    to make the movement more natural, and adjusted placement so they don’t
    overlap. Adjusted script to remove unnecessary variables.

--- ICS62/Assets/Scripts/FishBucket.cs
+++ ICS62/Assets/Scripts/FishBucket.cs
@@ -2,16 +2,14 @@
 using System.Collections;
 
 public class FishBucket : MonoBehaviour {
-		
-
-	public GameObject bucket;
 
 	void OnCollisionEnter(Collision other)
 		{
 		//if collision with dock, move this object above the bucket
 		if (other.collider.CompareTag ("Dock")) {
+			
+			this.transform.position = new Vector3(36.47f, 3.5f,-3.92f);
 			this.gameObject.transform.localScale = new Vector3 (.2f, .2f, .2f);
-			this.transform.position = new Vector3(36.47f, 2.5f,-3.92f);
 
 		}
 	}
